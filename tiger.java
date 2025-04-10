aa2f257fff785677938b142faba10309f4244183
prathyu <abcd@gmail.com>
2025-04-10 09:15:49 +0530

    hi2

--- tiger.java
+++ tiger.java
@@ -1,6 +1,6 @@
 class tiger{
     public static void main(String[] args) {
         System.out.println("Hello Tiger");
-        System.out.println("Hello Tiger ramsingh nayak ");
+        System.out.println("Hello Tiger ramsingh nayak 5 ");
     }
 }
